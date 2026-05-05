fadf7bfc56132548e3ef808573d729de2f6604bc
mahad <engmk123@gmail.com>
2026-05-05 23:07:30 +0200

    chore: add vercel analytics

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue'
 import { AppHeader } from "@src/components";
 import { RouterView } from "vue-router";
 </script>
@@ -7,6 +8,7 @@ import { RouterView } from "vue-router";
 	<div class="flex min-h-dvh flex-col bg-background text-foreground antialiased">
 		<AppHeader />
 		<main id="main" class="flex-1 min-h-0">
+      <Analytics />
 			<RouterView />
 		</main>
 	</div>
